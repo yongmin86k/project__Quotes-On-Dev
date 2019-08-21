1e0df13750abc25436efbd4edae869e493a42c10
Yongmin Kim <yongmin86k@gmail.com>
2019-08-21 15:37:51 -0700

    Add styles to 404 page

--- searchform.php
+++ searchform.php
@@ -1,10 +1,10 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<fieldset>
+	
 		<label>
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
-		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
+		<button class="search-submit search-icon">
+			<i class="fas fa-search"></i>
 		</button>
-	</fieldset>
+	
 </form>
