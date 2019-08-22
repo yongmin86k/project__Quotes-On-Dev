a2acf1d6742a16138ea26a20a835e03a337c3ac7
Yongmin Kim <yongmin86k@gmail.com>
2019-08-22 10:04:02 -0700

    Remove post navigations on single.php and add styles

--- single.php
+++ single.php
@@ -10,11 +10,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<header>
+			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+		</header>
+		
+
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
-			<?php the_post_navigation(); ?>
+			<?php get_template_part( 'template-parts/content' ); ?>
 
 		<?php endwhile; // End of the loop. ?>
 
